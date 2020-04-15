15ef6385cdfa8bba446cc6834e2439a15492c287
Thomas Moulard <tmoulard@amazon.com>
2020-04-15 09:39:44 -0700

    Fix CodeCov coverage generation (#32)
    Previously, the codecov invokation was silently failling as git
    is not installed on docker instances.
    
    Signed-off-by: Thomas Moulard <tmoulard@amazon.com>

--- .github/workflows/build-and-test.sh
+++ .github/workflows/build-and-test.sh
@@ -9,4 +9,6 @@ npm test
 
 # Upload code coverage to CodeCov, but do not fail the CI if CodeCov upload
 # fails as the service is sometimes flaky.
-codecov -f ./coverage/coverage-final.json || true
+codecov -f ./coverage/coverage-final.json \
+    --disable=detect --commit="${GITHUB_SHA}" \
+    --branch="${GITHUB_REF#refs/heads/}"
